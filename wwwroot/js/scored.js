9dc77a9724c4cb1be5f78472e2c689aa2f9d826f
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2019-11-15 03:36:40 -0300

    exibir rating

---
+++ wwwroot/js/scored.js
@@ -0,0 +1,37 @@
+
+
+function myFunction(estrela) {
+    let scored = estrela.dataset;
+    if(scored.rating>=1){
+        estrela.src="../Imagem/fill-estrela.png";
+    }   
+ }
+ 
+ function myFunction2(estrela) {
+     let scored = estrela.dataset;
+     if(scored.rating>=2){
+         estrela.src="../Imagem/fill-estrela.png";
+     }   
+ }
+ 
+ function myFunction3(estrela) {
+     let scored = estrela.dataset;
+     if(scored.rating>=3){
+         estrela.src="../Imagem/fill-estrela.png";
+     }   
+ }
+ 
+  function myFunction4(estrela) {
+     let scored = estrela.dataset;
+     if(scored.rating>=4){
+         estrela.src="../Imagem/fill-estrela.png";
+     }   
+ }
+ 
+ function myFunction5(estrela) {
+     let scored = estrela.dataset;
+     if(scored.rating>=5){
+         estrela.src="../Imagem/fill-estrela.png";
+     }   
+ }
+ 
