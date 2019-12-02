1313a6b187d91360b39b806eb8d7ea6b0e775c7e
ferrazeduardo <eduardo.borges.ferraz@gmail.com>
2019-12-01 21:34:44 -0300

    pedido detalhe

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -2,6 +2,10 @@ namespace Ecommerce.Models
 {
     public class Pedido
     {
-        
+        public int PedidoId { get; set; }
+        public string Nome { get; set; }
+        public string Quantidade { get; set; }
+        public string Preco { get; set; }
+        public Produto Produto{ get; set;}
     }
 }
